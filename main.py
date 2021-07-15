145e84fa6014244e506f3eb68f5db713c3ed7876
ErizoRocks <67878205+ErizoRocks@users.noreply.github.com>
2021-07-15 17:03:01 +0000

    Added restart

--- main.py
+++ main.py
@@ -6,28 +6,36 @@ from ship import Ship
 from asteroid import Asteroid
 import random
 import json
+import time
 
 pygame.init()
 screen_info = pygame.display.Info()
 width = screen_info.current_w
 height = screen_info.current_h
 size = (width, height)
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 clock = pygame.time.Clock()
-player = Ship((width // 2, height // 2))
-asteroids = pygame.sprite.Group()
-asteroid_count = 8
-Leveldata = json.load(open('game.json', 'rb'))
+# player = Ship((width // 2, height // 2))
+# asteroids = pygame.sprite.Group()
+# asteroid_count = 8
+asteroids = None
+asteroid_count = None
+player = None
 
 
 def init():
   global asteroid_count
   global asteroids
+  global player
+  player = Ship((width // 2, height // 2))
+  asteroids = pygame.sprite.Group()
+  asteroid_count = 8
   asteroids.empty()
   for i in range (asteroid_count):
-    asteroids.add(Asteroid(
-      (random.randint(50, width - 50), random.randint(50, height - 50)),random.randint(15, 120)
-    ))
+    ran_x = random.randint(50, width - 50)
+    ran_y = random.randint(50, height - 50)
+    ran_size = random.randint(15, 120)
+    asteroids.add(Asteroid((ran_x, ran_y), ran_size))
 
 def main():
   init()
@@ -65,8 +73,10 @@ def main():
       Heartsremaining = player.health // 20
       print("One heart lost. You have", Heartsremaining, "/5 hearts remaining.")
       if player.health == 0:
-       print("Game over. :(")
-       exit()
+        print("Game over. :(")
+        print("Restarting...")
+        time.sleep(3)
+        init()
       
     screen.blit(player.image, player.rect)
     pygame.display.update()
@@ -74,3 +84,7 @@ def main():
 if __name__ == '__main__':
   main()
 
+
+#  {
+#  "player": {"Level1":{"health": 40, "speed": 2},"Level2":{"health": 100, "speed": 5},"Level3":{"health": 200, "speed": 10}}, "asteroid":{"damage": 20}
+#  }
--- ship.py
+++ ship.py
@@ -16,3 +16,11 @@ class Ship(pygame.sprite.Sprite):
 
   def update(self):
     self.rect.move_ip(self.speed)
+    screen_info = pygame.display.Info()
+    self.rect.move_ip(self.speed)
+    if self.rect.left < 0 or self.rect.right > screen_info.current_w:
+      self.speed[0] = 0
+      self.rect.move_ip(self.speed[0], 0)
+    if self.rect.top < 0 or self.rect.bottom > screen_info.current_h:
+      self.speed[1] = 0
+      self.rect.move_ip(0, self.speed[1])
