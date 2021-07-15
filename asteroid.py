571273878b71e65fb85d8822d0c8498904587507
ErizoRocks <67878205+ErizoRocks@users.noreply.github.com>
2021-07-15 14:55:50 +0000

    Coded asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,23 @@
 import pygame
+import random
+
 class Asteroid(pygame.sprite.Sprite):
-  def __init__(self):
+
+  def __init__(self, pos, size):
     super().__init__()
-    pass
+    self.image = pygame.image.load('AsteroidImage.png')
+    self.image = pygame.transform.smoothscale(self.image, (size, size))
+    self.rect = self.image.get_rect()
+    self.rect.center = pos
+    self.speed = pygame.math.Vector2(0, 3)
+    self.speed.rotate_ip(random.randint(0, 360))
+
+  def update(self):
+    screen_info = pygame.display.Info()
+    self.rect.move_ip(self.speed)
+    if self.rect.left < 0 or self.rect.right > screen_info.current_w:
+      self.speed[0] *= -1
+      self.rect.move_ip(self.speed[0], 0)
+    if self.rect.top < 0 or self.rect.bottom > screen_info.current_h:
+      self.speed[1] *= -1
+      self.rect.move_ip(0, self.speed[1])
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas
 import sys
 from pygame.locals import *
 from ship import Ship
+from asteroid import Asteroid
+import random
 
 pygame.init()
 screen_info = pygame.display.Info()
@@ -12,8 +14,20 @@ size = (width, height)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 player = Ship((width // 2, height // 2))
+asteroids = pygame.sprite.Group()
+asteroid_count = 10
+
+def init():
+  global asteroid_count
+  global asteroids
+  asteroids.empty()
+  for i in range (asteroid_count):
+    asteroids.add(Asteroid(
+      (random.randint(50, width - 50), random.randint(50, height - 50)),random.randint(15, 120)
+    ))
 
 def main():
+  init()
   while True:
     clock.tick(60)
     for event in pygame.event.get():
@@ -21,13 +35,13 @@ def main():
         sys.exit(0)
       if event.type == pygame.KEYDOWN:
         if event.key ==   pygame.K_LEFT:
-          player.speed[0] = -50
+          player.speed[0] = -2
         elif event.key ==   pygame.K_RIGHT:
-          player.speed[0] = +50
+          player.speed[0] = +2
         elif event.key ==   pygame.K_DOWN:
-          player.speed[1] = 50
+          player.speed[1] = 2
         elif event.key ==   pygame.K_UP:
-          player.speed[1] = -50  
+          player.speed[1] = -2  
       if event.type == pygame.KEYUP:
         if event.key ==   pygame.K_LEFT:
           player.speed[0] = 0  
@@ -37,8 +51,10 @@ def main():
           player.speed[1] = 0
         elif event.key ==   pygame.K_UP:
           player.speed[1] = 0      
-    player.update()
     screen.fill((0, 0, 0))
+    player.update()
+    asteroids.update()
+    asteroids.draw(screen)
     screen.blit(player.image, player.rect)
     pygame.display.update()
 
